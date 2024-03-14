23ed4e598859ff62c0fa826cbf2efe4b88002ed7
Thiago Ribeiro <thiagodeev@gmail.com>
2024-03-14 18:53:17 -0300

    created hardhat project

--- server/src/routes/chainId.ts
+++ server/src/routes/chainId.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 let chainId = Router();
 
-chainId.post('/', async (req, res, next) => {
+chainId.get('/', async (req, res, next) => {
   const response = "Hello World!"
   console.log('First log of the operation.')
   res.status(200).json(response)
